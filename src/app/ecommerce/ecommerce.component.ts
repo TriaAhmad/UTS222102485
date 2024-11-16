4b81b4368ba849d2477aa409d492685275c81a25
TriaAhmad <matchakubawa@gmail.com>
2024-11-16 09:01:35 +0700

    update

---
+++ src/app/ecommerce/ecommerce.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-ecommerce',
+  standalone: true,
+  imports: [],
+  templateUrl: './ecommerce.component.html',
+  styleUrl: './ecommerce.component.css'
+})
+export class EcommerceComponent {
+
+}
